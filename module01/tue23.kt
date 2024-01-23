c64ab710456fd40e5ead0fb8b826d526a9cb8280
angelc258 <157508336+angelc258@users.noreply.github.com>
2024-01-23 11:32:43 -0600

    Created Module 1 Code

---
+++ module01/tue23.kt
@@ -0,0 +1,16 @@
+fun main () {
+    //println("Hello, world")
+    //println("My name is Angel and I am learning Kotlin")
+    println("The hero announces her presence to the world")
+    
+    var heroName: String = "Madrigal"
+    println(heroName)
+    
+    
+    var playerLevel: Int = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel +- 1
+    println(playerLevel)
+}
