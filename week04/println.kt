ccd6d8641611c1ddf441bed0b0e546a3070a29e2
angelc258 <157508336+angelc258@users.noreply.github.com>
2024-02-01 09:55:22 -0600

    reated println code

---
+++ week04/println.kt
@@ -0,0 +1,11 @@
+// HelloWorld/Task1.kt
+package helloWorldITSE1329
+
+// TODO
+
+fun main() {
+    println("Hello, World!")
+    println("My name is Angel C")
+    println("I am taking Kotlin ITSE-1329")
+    
+}
