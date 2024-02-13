473325582153444e167d5aea31a4823211f61ab0
angelc258 <157508336+angelc258@users.noreply.github.com>
2024-02-13 11:02:04 -0600

    salary

---
+++ module04/salary.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var name: String = "Joe Mahomey"
+    var age: Int = 99
+    var annualPay: Double = 999999.99
+    
+    println("My Name is Joe Mahomey " + name + ", my age is" + age +".")
+}
