b37ab6c5dd45b506f1e666d3169d68408501b96f
angelc258 <157508336+angelc258@users.noreply.github.com>
2024-02-15 10:38:20 -0600

    marketing

---
+++ week04/_review/marketing.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    
+    val Friends = 500
+    if (Friends < 500) {
+        println("Adopt A Dog Today")
+    } else {
+        println("Buy Cat Food at 20% Off.")
+    }
+}
